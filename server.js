25914f89d4137545dc776f668e2e2a9be9742465
= <=>
2022-09-22 00:00:25 -0300

    fix express server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'))
+app.use(express.static(__dirname + '/dist/tcc-controle-academico-angular'))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/tcc-controle-academico-angular/index.html'))

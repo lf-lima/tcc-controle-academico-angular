2ea5682de98c8e6ad9a055fc69e014d3d5dfb344
= <=>
2022-09-28 09:22:08 -0300

    adjust some names and permission to create students

--- src/app/shared/services/token.service.ts
+++ src/app/shared/services/token.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core'
+import jwtDecode from 'jwt-decode'
 
 @Injectable({ providedIn: 'root' })
 export class TokenService {
-  private authToken: string = 'authToken'
+  private tokenId: string = 'authToken'
+  private permissionsId: string = 'userPermissions'
 
   getToken (): string | null {
-    return window.localStorage.getItem(this.authToken)
+    return window.localStorage.getItem(this.tokenId)
+  }
+
+  getPermissions (): string[] {
+    const permissionsString = window.localStorage.getItem(this.permissionsId) as string
+    return JSON.parse(permissionsString)
   }
 
   setToken (token: string): void {
-    window.localStorage.setItem(this.authToken, token)
+    window.localStorage.setItem(this.tokenId, token)
+
+    const permissions = this.getPermissionsFromToken(token)
+
+    window.localStorage.setItem(this.permissionsId, JSON.stringify(permissions))
   }
 
   removeToken (): void {
-    window.localStorage.removeItem(this.authToken)
+    window.localStorage.removeItem(this.tokenId)
+  }
+
+  getPermissionsFromToken (token: string) {
+    const decode = jwtDecode(token) as any
+    return decode.permissions
   }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { AuthService } from 'src/app/shared/services/auth.service'
+import { TokenService } from 'src/app/shared/services/token.service'
 
 @Component({
   selector: 'app-sidebar',
@@ -9,12 +10,25 @@ import { AuthService } from 'src/app/shared/services/auth.service'
 export class SidebarComponent implements OnInit {
 
   constructor (
-    private authService: AuthService
+    private authService: AuthService,
+    private tokenService: TokenService
   ) { }
 
   count = 0
+  permissions!: string[]
 
   ngOnInit (): void {
+    this.permissions = this.tokenService.getPermissions()
+  }
+
+  checkHasPermission (requiredPermission: string): boolean {
+    return !!this.permissions.find(permission => permission === requiredPermission)
+  }
+
+  checkHasChildren (test: any): boolean {
+    console.log(test)
+
+    return true
   }
 
   logout () {
